45af979f7182ef392a5d43d40914b45ea6171732
Andreadlc <andreadelcros7@gmail.com>
2026-02-19 17:11:33 +0100

    ajout d'un historique

--- controllers/justeprixControler.js
+++ controllers/justeprixControler.js
@@ -1,4 +1,9 @@
 exports.getJustePrix = (req, res) => {
-    res.render('justeprix', { isLog: req.session.isLog,userRole: req.session.user.role });
-}
+    // On vérifie si l'utilisateur existe avant de demander son rôle
+    const role = (req.session.user && req.session.user.role) ? req.session.user.role : 'guest';
 
+    res.render('justeprix', { 
+        isLog: req.session.isLog, 
+        userRole: role 
+    });
+}
